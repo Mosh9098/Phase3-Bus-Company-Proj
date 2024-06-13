f1a2eba85df98ce9636eb97388cf7c10da22097a
mosh9098 <mosh9098@gmail.com>
2024-06-13 23:01:42 +0300

    connection comments

--- lib/db/connection.py
+++ lib/db/connection.py
@@ -1,8 +1,15 @@
 import sqlite3
 
+# Specify the path to the SQLite database file
 DATABASE_NAME = './db/BusCompany.db'
 
+# Function to establish a connection to the database
 def get_db_connection():
+    # Connect to the SQLite database using the specified database file
     conn = sqlite3.connect(DATABASE_NAME)
+    
+    # Set the row factory to sqlite3.Row to access columns by name
     conn.row_factory = sqlite3.Row
+    
+    # Return the connection object
     return conn
