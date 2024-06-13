84a1371dad580153e67e5ce3375471cd85c5f7c1
mosh9098 <mosh9098@gmail.com>
2024-06-13 22:48:47 +0300

    route comments

--- lib/models/route.py
+++ lib/models/route.py
@@ -2,26 +2,32 @@ from db.connection import get_db_connection
 
 class Route:
     def __init__(self, route_id, route_name):
+        # Initialize the Route instance with the provided attributes
         self._route_id = route_id
         self._route_name = route_name
 
     def __repr__(self):
+        # Define the string representation of the Route instance
         return f'<Route {self.route_name}>'
 
+    # Define the route_id property with a getter method
     @property
     def route_id(self):
         return self._route_id
 
+    # Define the route_name property with a getter method
     @property
     def route_name(self):
         return self._route_name
 
+    # Define the setter method for the route_name property
     @route_name.setter
     def route_name(self, value):
         if not isinstance(value, str) or not value:
             raise ValueError("Route name must be a non-empty string")
         self._route_name = value
 
+    # Define a class method to add a new Route to the database
     @classmethod
     def add(cls, route_name):
         conn = get_db_connection()
@@ -30,6 +36,7 @@ class Route:
         conn.commit()
         conn.close()
 
+    # Define a class method to retrieve all Route instances from the database
     @classmethod
     def get_all(cls):
         conn = get_db_connection()
@@ -39,6 +46,7 @@ class Route:
         conn.close()
         return [cls(route['RouteID'], route['RouteName']) for route in routes]
 
+    # Define an instance method to update the current Route's information in the database
     def update(self, route_name):
         conn = get_db_connection()
         cursor = conn.cursor()
@@ -46,6 +54,7 @@ class Route:
         conn.commit()
         conn.close()
 
+    # Define an instance method to delete the current Route from the database
     def delete(self):
         conn = get_db_connection()
         cursor = conn.cursor()
